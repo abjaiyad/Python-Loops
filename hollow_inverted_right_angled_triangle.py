6ccee5f771b325b635d0f3da7be9d7fcc4b70142
Amad Bin Jaiyad <amadbinjaiyad2005@gmail.com>
2026-09-12 20:52:12 +0530

    Add hollow inverted right-angled triangle program

---
+++ hollow_inverted_right_angled_triangle.py
@@ -0,0 +1,11 @@
+# Hollow Inverted Right-Angled Triangle
+
+n = int(input("Enter size: "))
+
+for i in range(n):
+    for j in range(n - i):
+        if (i == 0) or (j == 0 or j == n - i - 1):
+            print("*", end="")
+        else:
+            print(" ", end="")
+    print()
